09399788c438fc608c0942ce19558a7f6ebd1cfb
gazizkv <valitovgaziz@gmail.com>
2022-01-09 13:40:02 +0500

    Bubble sorting array is done.

--- java/src/Bubble.java
+++ java/src/Bubble.java
@@ -5,6 +5,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Bubble {
     public static void main(String[] args) {
@@ -16,6 +17,29 @@ public class Bubble {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(readLine);
+        assert readLine != null;
+        String[] array = readLine.split(" ");
+        int[] integers = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            integers[i] = Integer.parseInt(array[i]);
+        }
+
+        System.out.println("Non sorted entered array " + Arrays.toString(integers));
+        int num = Integer.MIN_VALUE;
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < integers.length - 1; i++) {
+                if (integers[i] > integers[i + 1]) {
+                    num = integers[i];
+                    integers[i] = integers[i + 1];
+                    integers[i + 1] = num;
+                    isSorted = false;
+                }
+            }
+        }
+
+        System.out.println("Sorted array " + Arrays.toString(integers));
+
     }
 }
